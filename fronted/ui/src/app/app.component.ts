ab14755db626b5bc6b4953a68d4ea5a5b90f4e85
GuyRobot <bluexker@gmail.com>
2022-12-17 15:10:21 +0700

    Add basic auth method

--- fronted/ui/src/app/app.component.ts
+++ fronted/ui/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(public oidcSecurityService: OidcSecurityService) { }
+
   title = 'ui';
+
+  ngOnInit(): void {
+    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData, accessToken, idToken }) => {
+      /*...*/
+    });
+  }
+
+  login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout() {
+    this.oidcSecurityService.logoff().subscribe((result) => console.log(result));
+  }
 }
