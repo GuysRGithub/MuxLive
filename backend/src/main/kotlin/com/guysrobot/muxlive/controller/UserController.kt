069400f8fd314f7a63850591e7ad9f8fa6107956
GuyRobot <bluexker@gmail.com>
2022-12-20 11:21:29 +0700

    Fix wrong mapping method for regsiter (get -> post)

--- backend/src/main/kotlin/com/guysrobot/muxlive/controller/UserController.kt
+++ backend/src/main/kotlin/com/guysrobot/muxlive/controller/UserController.kt
@@ -3,7 +3,7 @@ package com.guysrobot.muxlive.controller
 import com.guysrobot.muxlive.service.UserService
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/users")
 class UserController(private val userService: UserService) {
 
-    @GetMapping("register")
+    @PostMapping("register")
     fun register(authentication: Authentication): String {
         val jwt = authentication.principal as? Jwt ?: return "Failed to registration"
         userService.register(jwt.tokenValue)
